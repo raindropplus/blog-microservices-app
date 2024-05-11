3f011c859978261b7aec2efb66b734d8a93dc1d6
raindropplus <raindropplus11@gmail.com>
2024-05-12 01:04:54 +0600

    add event bus

--- posts/index.js
+++ posts/index.js
@@ -2,25 +2,37 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors')
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors())
 
-const posts={};
+const posts = {};
 
-app.get('/posts',(req, res)=>{
+app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts',(req, res)=>{
-    const id = randomBytes(4).toString('hex');
-    const { tittle } = req.body;
-    posts[id]={id,tittle};
-    res.status(201).send(posts[id]);
+app.post('/posts', async (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { tittle } = req.body;
+  posts[id] = { id, tittle };
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: { id, tittle }
+  })
+
+  res.status(201).send(posts[id]);
 
 });
 
-app.listen(4000, ()=>{
-    console.log('Listening on 4000');
+app.post('/events', (req,res)=>{
+  console.log('Received Event', req.body.type);
+  res.send({});
+})
+
+app.listen(4000, () => {
+  console.log('Listening on 4000');
 })
